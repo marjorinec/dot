bbca77769dbd3ca2068d724bc42fae851b1da8d4
alline.coelho <alline.coelho@Administrators-MacBook-Pro.local>
2017-04-22 20:21:15 -0300

    aaaaaaaaaaaaaaaaa

--- times/models.py
+++ times/models.py
@@ -7,3 +7,6 @@ class Time(models.Model):
     nome = models.CharField(max_length=20)
     link = models.CharField(max_length=20)
     email = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.nome
